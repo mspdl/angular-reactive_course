b9a229e9e849219a32184566ce6201b8635aeb32
Morgan <morgan.espindola@gmail.com>
2023-07-25 19:40:09 -0300

    Changed list of courses to a better component

--- course_project/src/app/services/courses.service.ts
+++ course_project/src/app/services/courses.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Course } from "../model/course";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, shareReplay } from "rxjs/operators";
+import { Course } from "../model/course";
 
 @Injectable({
   providedIn: "root",
@@ -11,8 +11,9 @@ export class CoursesService {
   constructor(private http: HttpClient) {}
 
   loadAllCourses(): Observable<Course[]> {
-    return this.http
-      .get<Course[]>("/api/courses")
-      .pipe(map((res) => res["payload"]));
+    return this.http.get<Course[]>("/api/courses").pipe(
+      map((res) => res["payload"]),
+      shareReplay()
+    );
   }
 }
