d900b6ac922af8c5bf4493cd5204fe0e69c1aa2f
Morgan <morgan.espindola@gmail.com>
2023-08-26 21:26:30 -0300

    Section 03: Implemented message component on course dialog

--- course_project/server/save-course.route.ts
+++ course_project/server/save-course.route.ts
@@ -1,25 +1,23 @@
-import {Request, Response} from 'express';
-import {COURSES} from "./db-data";
-import {setTimeout} from 'timers';
-
+import { Request, Response } from "express";
+import { setTimeout } from "timers";
+import { COURSES } from "./db-data";
 
 export function saveCourse(req: Request, res: Response) {
+  const ERROR_MODE = false;
 
-  /*
-  console.log("ERROR saving course!");
-  res.sendStatus(500);
-  return;
-
-  */
-
+  if (ERROR_MODE) {
+    console.log("ERROR saving course!");
+    res.sendStatus(500);
+    return;
+  } else {
     const id = req.params["id"],
-        changes = req.body;
+      changes = req.body;
 
     console.log("Saving course changes", id, JSON.stringify(changes));
 
     const newCourse = {
       ...COURSES[id],
-      ...changes
+      ...changes,
     };
 
     COURSES[id] = newCourse;
@@ -27,11 +25,7 @@ export function saveCourse(req: Request, res: Response) {
     console.log("new course version", newCourse);
 
     setTimeout(() => {
-
-        res.status(200).json(COURSES[id]);
-
+      res.status(200).json(COURSES[id]);
     }, 2000);
-
-
-
+  }
 }
--- course_project/src/app/course-dialog/course-dialog.component.ts
+++ course_project/src/app/course-dialog/course-dialog.component.ts
@@ -2,7 +2,10 @@ import { AfterViewInit, Component, Inject } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import * as moment from "moment";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 import { LoadingService } from "../loading/loading.service";
+import { MessagesService } from "../messages/messages.service";
 import { Course } from "../model/course";
 import { CoursesService } from "../services/courses.service";
 
@@ -10,7 +13,7 @@ import { CoursesService } from "../services/courses.service";
   selector: "course-dialog",
   templateUrl: "./course-dialog.component.html",
   styleUrls: ["./course-dialog.component.css"],
-  providers: [LoadingService],
+  providers: [LoadingService, MessagesService],
 })
 export class CourseDialogComponent implements AfterViewInit {
   form: FormGroup;
@@ -22,7 +25,8 @@ export class CourseDialogComponent implements AfterViewInit {
     private dialogRef: MatDialogRef<CourseDialogComponent>,
     private coursesService: CoursesService,
     @Inject(MAT_DIALOG_DATA) course: Course,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private messagesService: MessagesService
   ) {
     this.course = course;
 
@@ -38,7 +42,16 @@ export class CourseDialogComponent implements AfterViewInit {
 
   save() {
     const changes = this.form.value;
-    const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes);
+    const saveCourse$ = this.coursesService
+      .saveCourse(this.course.id, changes)
+      .pipe(
+        catchError((error) => {
+          const message = "Could not save the course";
+          this.messagesService.showErrors(message);
+          console.log("message:", message, "\nerror: ", error);
+          return throwError(error);
+        })
+      );
     this.loadingService.isLoading(saveCourse$).subscribe((val) => {
       this.dialogRef.close(val);
     });
