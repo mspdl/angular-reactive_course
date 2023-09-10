f4720a493f8f0a9890fc0edb3dac87a8f6af1523
Morgan <morgan.espindola@gmail.com>
2023-09-10 20:22:07 -0300

    Section 04 - Authentication Store - Browser Refresh support with Local Storage

--- course_project/src/app/services/auth.store.ts
+++ course_project/src/app/services/auth.store.ts
@@ -4,6 +4,8 @@ import { BehaviorSubject, Observable } from "rxjs";
 import { map, shareReplay, tap } from "rxjs/operators";
 import { User } from "../model/user";
 
+const AUTH_DATA = "auth_data";
+
 @Injectable({ providedIn: "root" })
 export class AuthStore {
   private subjectUser = new BehaviorSubject<User>(null);
@@ -15,16 +17,25 @@ export class AuthStore {
   constructor(private http: HttpClient) {
     this.isLoggedIn$ = this.user$.pipe(map((user) => !!user));
     this.isLoggedOut$ = this.isLoggedIn$.pipe(map((loggedIn) => !loggedIn));
+
+    const localUser = localStorage.getItem(AUTH_DATA);
+    if (localUser) {
+      this.subjectUser.next(JSON.parse(localUser));
+    }
   }
 
   login(email: string, password: string): Observable<User> {
     return this.http.post<User>("/api/login", { email, password }).pipe(
-      tap((user) => this.subjectUser.next(user)),
+      tap((user) => {
+        this.subjectUser.next(user);
+        localStorage.setItem(AUTH_DATA, JSON.stringify(user));
+      }),
       shareReplay()
     );
   }
 
   logout() {
     this.subjectUser.next(null);
+    localStorage.removeItem(AUTH_DATA);
   }
 }
